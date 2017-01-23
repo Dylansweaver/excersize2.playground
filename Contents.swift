b20f8b9bd8bbeb94f4672e3d9fbc17fa449e2286
Dylansweaver <dweaver@a110-m-test.mchenry.edu>
2017-01-23 15:49:57 -0600

    excersize2

---
+++ Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+class age {
+
+var str = "When 64"
+
+var birthdate = 1996
+    
+
+    
+
+
+}
+var age2 = age()
+var age64 = age2.birthdate + 64
+
+print("You Will Be 64 in the Year",age64)
